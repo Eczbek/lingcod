edd86ed8f18b28aded4138bd93228c3792ed86ef
user <bogdan@shmonov.me>
2022-01-15 16:17:57 -0500

    Added math.js

--- main/number.js
+++ main/number.js
@@ -8,26 +8,5 @@ import { typeOf } from './misc.js';
  * @returns {boolean}
  */
 export function isNumber (value) {
-	return typeOf(value) === 'Number' && !Number.isNaN(value) && Number.isFinite(value);
-}
-
-/**
- * Checks if number is even
- * @param {number} number 
- * @returns {boolean}
- */
-export function isEven (number) {
-	return number % 2 === 0;
-}
-
-/**
- * Checks if number is prime
- * @param {number} number 
- * @returns {boolean}
- */
-export function isPrime (number) {
-	for (let i = 2; i <= Math.sqrt(number); i++) {
-		if (number % i === 0) return false;
-	}
-	return number > 1;
+	return typeOf(value) === 'Number' && Number.isFinite(value);
 }
