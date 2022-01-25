4059742277d1075bdb1b0cd2097fbeb30672cc72
Eczbek <eczbek.void@gmail.com>
2022-01-25 07:55:28 -0500

    reorganized

---
+++ browser/misc.js
@@ -0,0 +1,25 @@
+
+class EventEmitter extends EventTarget {
+	#funcs = {};
+
+	emit = (id, ...data) => {
+		this.dispatchEvent(new CustomEvent(id, { detail: data }));
+	}
+
+	on = (id, callback) => {
+		this.#funcs[id] = ({ detail }) => callback(...detail);
+		this.addEventListener(id, this.#funcs[id]);
+	}
+
+	off = (id) => {
+		this.removeEventListener(id, this.#funcs[id]);
+		delete this.#funcs[id];
+	}
+
+	once = (id, callback) => {
+		this.on(id, (...args) => {
+			this.off(id);
+			callback(...args);
+		});
+	}
+}
---
+++ general/time.js
@@ -0,0 +1,27 @@
+
+export const WEEK_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+export const MILLIS_BEFORE_1970 = 6.212592e13;
+
+const timeFormatConfig = {
+	Y: { func: 'FullYear', pad: 4 },
+	M: { func: 'Month', add: 1, arr: MONTHS },
+	D: { func: 'Date' },
+	d: { func: 'Day', pad: 1, arr: WEEK_DAYS },
+	H: { func: 'Hours' },
+	m: { func: 'Minutes' },
+	S: { func: 'Seconds' },
+	s: { func: 'Milliseconds', pad: 4 },
+	T: { func: 'Time', pad: 0, now: true }
+};
+
+export const createTimeFormat = (format, prefix = '%', options = {}) => {
+	const prefix = (check) => check ? 'UTC' : '';
+	return (date = new Date(), utc = false, names = false) => prefix(utc) + format.replace(new RegExp(Object.keys(config).map((key) => prefix + key).join('|'), 'g'), (key) => {
+		const type = key.replace(new RegExp(prefix), '');
+		const { func, pad = 2, add = 0, arr, now = false } = { ...timeFormatConfig[type], ...options[type] };
+		const num = date[`get${prefix(utc && !now)}${func}`]();
+		return names && arr ? arr[num] : String(num + add).padStart(pad, 0);
+	});
+}
