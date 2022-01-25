006bebd8f068950d26f3b75b0c66a3eded5e7560
Eczbek <84922236+Eczbek@users.noreply.github.com>
2022-01-25 10:08:46 -0500

    deleted char.js
    (who needed those anyway?)

--- general/char.js
+++
@@ -1,7 +0,0 @@
-
-export const NUMBERS = '0123456789';
-export const LETTERS = 'abcdefghijklmnopqrstuvwxyz';
-export const SPECIALS = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-export const VOWELS = 'aeiou';
-export const SEMIVOWELS = 'rwy';
-export const CONSONANTS = 'bcdfghjklmnpqstvxz';
