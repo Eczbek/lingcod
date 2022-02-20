50e01b03b715e293a214eb838b7ad8136ca9a17c
Eczbek <eczbek.void@gmail.com>
2022-02-20 18:53:32 -0500

    Remove JSDoc

--- general/array.js
+++ general/array.js
@@ -2,21 +2,11 @@
 import { randInt } from './random.js';
 
 
-/**
- * Removes holes from array, does not remove nullish values
- * @param {Array<any>} array 
- * @returns {Array<any>}
- */
-export const compact = (array) => array.filter(() => true);
+export function compact (array) {
+	return array.filter(() => true);
+}
 
-/**
- * Creates an array of numbers, scaling in order from min to max
- * @param {number} max 
- * @param {number} min optional parameter
- * @param {number} step optional parameter
- * @returns {Array<number>}
- */
-export const range = (max, min = 0, step = 1) => {
+export function range (max, min = 0, step = 1) {
 	const arr = [];
 	for (let i = min; i < max; i += step) {
 		arr.push(i);
@@ -24,7 +14,7 @@ export const range = (max, min = 0, step = 1) => {
 	return arr;
 }
 
-export const createDenseArray = (length, fillCallback) => {
+export function createDenseArray (length, fillCallback) {
 	const arr = [];
 	for (let i = 0; i < length; ++i) {
 		arr.push(fillCallback(i));
@@ -32,43 +22,24 @@ export const createDenseArray = (length, fillCallback) => {
 	return arr;
 }
 
-/**
- * Checks if array contains several values
- * @param {Array<any>} array 
- * @param {Array<any>} values 
- * @returns {boolean}
- */
-export const includesAll = (array, values) => values.every((item) => array.includes(item));
+export function includesAll (array, values) {
+	return values.every((item) => array.includes(item));
+}
 
-/**
- * Filters items by how many times they appear in the array
- * @param {Array<any>} array 
- * @param {number} occurences 
- * @returns {Array<any>}
- */
-export const filterByOccurences = (array, occurences) => array.filter((item1) => array.filter((item2) => item1 === item2).length === occurences);
+export function filterByOccurences (array, occurences) {
+	return array.filter((item1) => array.filter((item2) => item1 === item2).length === occurences);
+}
 
-/**
- * Sorts items into groups
- * @param {Array<any>} array 
- * @param {Function} getGroup 
- * @param {Function} getValue optional parameter
- * @returns {Map}
- */
-export const group = (array, getGroup, getValue = (x) => x) => array.reduce((map, item, index) => {		const group = getGroup(item, index, [...array]);
-	if (!map.has(group)) map.set(group, []);
-	map.get(group).push(getValue(item, index));
-	return map;
-}, new Map());
+export function group (array, getGroup, getValue = (x) => x) {
+	return array.reduce((map, item, index) => {	
+		const group = getGroup(item, index, [...array]);
+		if (!map.has(group)) map.set(group, []);
+		map.get(group).push(getValue(item, index));
+		return map;
+	}, new Map());
+}
 
-/**
- * Splits array into several arrays
- * @param {Array<any>} array 
- * @param {Function} getSize 
- * @param {boolean} overflow optional parameter
- * @returns {Array<Array<any>>}
- */
-export const chunk = (array, getSize, overflow = false) => {
+export function chunk (array, getSize, overflow = false) {
 	const result = [];
 	while (true) {
 		const size = getSize(array[result.length], result.length, [...array]);
@@ -77,29 +48,11 @@ export const chunk = (array, getSize, overflow = false) => {
 	}
 }
 
-/**
- * Checks if all items in array are equal to value, or first item by default
- * @param {Array<any>} array 
- * @param {any} value optional parameter
- * @returns {boolean}
- */
-export const contentsAreEqual = (array, value = array[0]) => array.slice(1).every((item) => item === value);
+export function contentsAreEqual (array, value = array[0]) {
+	return array.slice(1).every((item) => item === value);
+}
 
-/**
- * Index filter callback
- * @callback indexFilterCallback
- * @param {any} value 
- * @param {number} index 
- * @returns {boolean}
- */
-
-/**
- * Filters indices
- * @param {Array<any>} array 
- * @param {indexFilterCallback} callback 
- * @returns {Array<number>}
- */
-export const filterIndices = (array, callback) => {
+export function filterIndices (array, callback) {
 	const result = [];
 	for (let i = 0; i < array.length; ++i) {
 		if (callback(array[i], i)) result.push(i);
@@ -107,56 +60,32 @@ export const filterIndices = (array, callback) => {
 	return result;
 }
 
-/**
- * Finds index of start of first sequence match
- * @param {Array<any>} array 
- * @param {Array<any>} sequence 
- * @param {boolean} loop optional parameter
- * @returns {number}
- */
-export const findIndexOfSequence = (array, sequence, loop = false) => array.findIndex((_, index1) => sequence.every((item, index2) => {
-	const index = index1 + index2;
-	return item === array[loop ? index % array.length : index];
-}));
+export function findIndexOfSequence (array, sequence, loop = false) {
+	return array.findIndex((_, index1) => sequence.every((item, index2) => {
+		const index = index1 + index2;
+		return item === array[loop ? index % array.length : index];
+	}));
+}
 
-/**
- * Finds indices of starts of all sequence matches
- * @param {Array<any>} array 
- * @param {Array<any>} sequence 
- * @param {boolean} loop optional parameter
- * @returns {Array<number>}
- */
-export const findIndicesOfSequence = (array, sequence, loop = false) => filterIndices(array, (_, index1) => sequence.every((item, index2) => {
-	const index = index1 + index2;
-	return item === array[loop ? index % array.length : index];
-}));
+export function findIndicesOfSequence (array, sequence, loop = false) {
+	return filterIndices(array, (_, index1) => sequence.every((item, index2) => {
+		const index = index1 + index2;
+		return item === array[loop ? index % array.length : index];
+	}));
+}
 
-/**
- * Finds empty indices, or "holes"
- * @param {Array<any>} array 
- * @returns {Array<number>}
- */
-export const findEmptyIndices = (array) => filterIndices(array, (_, index) => !Object.hasOwn(array, index));
+export function findEmptyIndices (array) {
+	return filterIndices(array, (_, index) => !Object.hasOwn(array, index));
+}
 
-/**
- * Checks if array contains holes
- * @param {Array<any>} array 
- * @returns {boolean}
- */
-export const isSparse = (array) => {
+export function isSparse (array) {
 	for (let i = 0; i < array.length; ++i) {
 		if (!Object.hasOwn(array, i)) return true;
 	}
 	return false;
 }
 
-/**
- * Swaps items by index, modifies passed array
- * @param {Array<any>} array 
- * @param {Array<number>} indices 
- * @returns {Array<any>}
- */
-export const swap = (array, indices) => {
+export function swap (array, indices) {
 	for (let i = 0; i < indices.length - 1; ++i) {
 		const prev = indices.at(i - 1);
 		[array[indices[i]], array[prev]] = [array[prev], array[indices[i]]];
@@ -164,25 +93,14 @@ export const swap = (array, indices) => {
 	return array;
 }
 
-/**
- * Shuffles items randomly, modifies passed array
- * @param {Array<any>} array 
- * @returns {Array<any>}
- */
-export const shuffle = (array) => {
+export function shuffle (array) {
 	for (let i = array.length - 1; i > 0; --i) {
 		swap(array, [i, randInt(i)]);
 	}
 	return array;
 }
 
-/**
- * Creates array of duplicate items
- * @param {any} value 
- * @param {number} count 
- * @returns {Array<any>}
- */
-export const multiply = (value, count) => {
+export function multiply (value, count) {
 	const result = [];
 	for (let i = 0; i < count; ++i) {
 		result.push(value);
--- general/logger.js
+++ general/logger.js
@@ -3,44 +3,22 @@ export default class Logger {
 	#id;
 	#timers = {};
 
-	/**
-	 * Creates logger with ID
-	 * @param {string} id 
-	 */
 	constructor (id) {
 		this.#id = id;
 	}
 
-	/**
-	 * Gets logger ID
-	 * @returns {string}
-	 */
 	getId () {
 		return this.#id;
 	}
 
-	/**
-	 * Logs data to console, with current ISO time, logger ID, and tag
-	 * @param {any} data 
-	 * @param {string} tag optional parameter
-	 */
 	log (data, tag = 'log') {
 		console.log(`[${new Date().toISOString()}] ${this.#id} (${tag.toUpperCase()}) `, data);
 	}
 
-	/**
-	 * Adds a timer
-	 * @param {string} id 
-	 */
 	addTimer (id) {
 		this.#timers.set(id, Date.now());
 	}
 
-	/**
-	 * Ends timer, logs and returns difference
-	 * @param {string} id 
-	 * @returns {number}
-	 */
 	endTimer (id) {
 		if (!this.#timers[id]) return;
 		const diff = Date.now() - this.#timers[id];
