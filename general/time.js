d3e7f390d0c19916f3f6ec5a2c6cba8aff755be8
user <bogdan@shmonov.me>
2022-03-22 07:45:36 -0400

    Remove very useless functions

--- general/time.js
+++ general/time.js
@@ -4,28 +4,6 @@ export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', '
 
 export const MILLIS_BEFORE_1970 = 6.212592e13;
 
-export function destructDate (date = new Date()) {
-	return {
-		year: date.getFullYear(),
-		month: date.getMonth(),
-		date: date.getDate(),
-		day: date.getDay(),
-		hours: date.getHours(),
-		minutes: date.getMinutes(),
-		seconds: date.getSeconds(),
-		milliseconds: date.getMilliseconds(),
-		yearUTC: date.getUTCFullYear(),
-		monthUTC: date.getUTCMonth(),
-		dateUTC: date.getUTCDate(),
-		dayUTC: date.getUTCDay(),
-		hoursUTC: date.getUTCHours(),
-		minutesUTC: date.getUTCMinutes(),
-		secondsUTC: date.getUTCSeconds(),
-		millisecondsUTC: date.getUTCMilliseconds(),
-		time: date.getTime()
-	};
-}
-
 const timeFormatConfig = {
 	Y: { func: 'FullYear', pad: 4 },
 	M: { func: 'Month', add: 1, arr: MONTHS },
