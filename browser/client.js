06b4017008033a058cd60dc637c7d003ededd6d2
user <bogdan@shmonov.me>
2022-03-22 07:42:09 -0400

    Upgrade WebSocketClient

--- browser/client.js
+++ browser/client.js
@@ -1,20 +1,40 @@
 
-class WebSocketClient extends EventEmitter {
+class WebSocketClient extends MinimalEventEmitter {
+	constructor (protocol = 'ws') {
+		super();
+		this.protocol = protocol;
+	}
+
+	protocol;
 	#socket;
+	reconnectTimeout = 15000;
 
-	connect (url = this.getURL()) {
-		this.#socket = new WebSocket(url);
+	connect (url) {
+		this.#socket = new WebSocket(url.replace(/^(.*:\/\/)?/, this.protocol + '://'));
 		this.#socket.addEventListener('open', () => this.emit('open'));
-		this.#socket.addEventListener('close', () => this.emit('close'));
-		this.#socket.addEventListener('message', ({ data }) => this.emit('data', data));
+		this.#socket.addEventListener('close', () => {
+			if (this.reconnectTimeout) setTimeout(() => this.connect(this.getURL()), this.reconnectTimeout);
+			this.emit('close');
+		});
+		this.#socket.addEventListener('message', ({ data }) => {
+			this.emit('data', data);
+			try {
+				this.emit('json', JSON.parse(data));
+			} catch {}
+		});
+		this.#socket.addEventListener('error', (error) => this.emit('error', error));
+
+		return this;
 	}
 
-	disconnect () {
-		this.#socket?.close();
+	disconnect (code, reason) {
+		this.#socket?.close(code, reason);
+		return this;
 	}
 
-	send (data) {
-		this.#socket?.send(JSON.stringify(data));
+	send (message) {
+		this.#socket?.send(JSON.stringify(message));
+		return this;
 	}
 
 	getURL () {
