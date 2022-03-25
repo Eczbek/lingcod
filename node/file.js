d6cd5f72904d6a1a8cae96f38f02eaabd7d083ad
user <bogdan@shmonov.me>
2022-03-25 10:33:10 -0400

    Remove server.js

--- node/file.js
+++
@@ -1,7 +0,0 @@
-
-import fsPromises from 'fs/promises';
-
-
-export async function readJSONConfigDir (path) {
-	return Object.fromEntries(await Promise.all((await fsPromises.readdir(path)).map(async (file) => [file.split('.')[0], JSON.parse(await fsPromises.readFile(`${path}/${file}`))])));
-}
