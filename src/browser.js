9f69f196f3f58fdb5416e0a8c5456ce9bfef90fd
Eczbek <eczbek.void@gmail.com>
2022-03-26 16:38:58 -0400

    Merge directories & rename files

--- src/browser.js
+++ src/browser.js
@@ -1,4 +1,32 @@
 
+class MinimalEventEmitter extends EventTarget {
+	#callbacks = {};
+
+	emit (id, ...data) {
+		this.dispatchEvent(new CustomEvent(id, { detail: data }));
+		return this;
+	}
+
+	on (id, callback) {
+		this.#callbacks[id] = ({ detail }) => callback(...detail);
+		this.addEventListener(id, this.#callbacks[id]);
+		return this;
+	}
+
+	off (id) {
+		this.removeEventListener(id, this.#callbacks[id]);
+		delete this.#callbacks[id];
+		return this;
+	}
+
+	once (id, callback) {
+		return this.on(id, (...args) => {
+			this.off(id);
+			callback(...args);
+		});
+	}
+}
+
 class MinimalWebSocketClient extends MinimalEventEmitter {
 	constructor (protocol = 'ws') {
 		super();
@@ -40,4 +68,23 @@ class MinimalWebSocketClient extends MinimalEventEmitter {
 	getURL () {
 		return this.#socket?.url;
 	}
+}
+
+function download (filename, data, type = 'text') {
+	const link = document.createElement('a');
+	link.download = filename;
+	link.href = URL.createObjectURL(new Blob([data], { type }));
+	link.click();
+}
+
+function animate (callback) {
+	function step () {
+		callback();
+		requestAnimationFrame(step);
+	}
+	requestAnimationFrame(step);
+}
+
+function getCookiesObject () {
+	return Object.fromEntries(document.cookie.split(';').map((entry) => entry.split('=')));
 }
