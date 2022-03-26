09033abccaa8811ffa50808daac5939cbda94d00
Eczbek <eczbek.void@gmail.com>
2022-03-26 09:22:48 -0400

    Add isEmail

--- general/string.js
+++ general/string.js
@@ -7,14 +7,6 @@ export function decapitalize (string) {
 	return string[0].toLowerCase() + string.slice(1);
 }
 
-export function spliceString (string, index, remove = 0, insert = '') {
-	return string.slice(0, index) + insert + string.slice(index + remove);
-}
-
-export function truncateString (string, length, replace = '...') {
-	return string.length > length ? string.slice(0, length - replace.length) + replace : string;
-}
-
 export function toPascalCase (string) {
 	return string.split(' ').map(capitalize).join('');
 }
@@ -29,4 +21,18 @@ export function toSnakeCase (string) {
 
 export function toKebabCase (string) {
 	return string.split(' ').map((word) => word.toLowerCase()).join('-');
+}
+
+export function spliceString (string, index, remove = 0, insert = '') {
+	return string.slice(0, index) + insert + string.slice(index + remove);
+}
+
+export function truncateString (string, length, replace = '...') {
+	return string.length > length
+		? string.slice(0, length - replace.length) + replace
+		: string;
+}
+
+export function isEmail (string) {
+	return /^(([a-z\d!#$%&'*+\/=?^_`{|}~-]+(\.[a-z\d!#$%&'*+\/=?^_`{|}~-]+)*)|(".+"))@([a-z\d-]+(\.[a-z\d]+)+|\[\d{1,3}(\.\d{1,3}){3}])$/i.test(string);
 }
