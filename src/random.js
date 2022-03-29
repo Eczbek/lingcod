a72af68b79d64bf379f9eb4bafbac88a42ad0e0b
Eczbek <eczbek.void@gmail.com>
2022-03-29 08:06:56 -0400

    Allow randFloat function inclusiveness

--- src/random.js
+++ src/random.js
@@ -2,12 +2,12 @@
 import { lerp } from './math.js';
 
 
-export function randFloat (max = 1, min = 0) {
-	return lerp(Math.random(), max, min);
+export function randFloat (max = 1, min = 0, inclusive = false) {
+	return lerp(Math.random(), max + inclusive, min);
 }
 
-export function randInt (max = Number.MAX_SAFE_INTEGER, min) {
-	return Math.floor(randFloat(max, min));
+export function randInt (max = Number.MAX_SAFE_INTEGER, min, inclusive) {
+	return Math.floor(randFloat(max, min, inclusive));
 }
 
 export function randBool () {
@@ -34,5 +34,5 @@ export function randHexColor (max = '#ffffff', min = '#000000') {
 	return `#${randString(6, (index) => {
 		const minNum = Number(`0x${min[index]}`);
 		return randInt(Math.max(Number(`0x${max[index]}`), minNum), minNum).toString(16);
-	})}`
+	})}`;
 }
