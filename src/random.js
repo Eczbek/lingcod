f805324374759c269e7351a8e1b536dd848c690b
Eczbek <eczbek.void@gmail.com>
2022-03-29 14:35:04 -0400

    Add documentation

--- src/random.js
+++ src/random.js
@@ -2,32 +2,87 @@
 import { lerp } from './math.js';
 
 
+/**
+ * Generates a random float
+ * @param {number?} max 
+ * @param {number?} min 
+ * @param {boolean?} inclusive 
+ * @returns {number}
+ */
 export function randFloat (max = 1, min = 0, inclusive = false) {
 	return lerp(Math.random(), max + inclusive, min);
 }
 
+/**
+ * Generates a random integer
+ * @param {number?} max 
+ * @param {number?} min 
+ * @param {boolean?} inclusive 
+ * @returns {number}
+ */
 export function randInt (max = Number.MAX_SAFE_INTEGER, min, inclusive) {
 	return Math.floor(randFloat(max, min, inclusive));
 }
 
+/**
+ * Generates a random boolean
+ * @returns {boolean}
+ */
 export function randBool () {
 	return !!randInt(2);
 }
 
+/**
+ * Picks a random item from an array
+ * @param {any[]} array 
+ * @returns {any}
+ */
 export function randItem (array) {
 	return array[randInt(array.length)];
 }
 
+/**
+ * Gets the array values for this index
+ * @callback getArrayValuesCallback
+ * @param {number} index 
+ * @returns {any[]}
+ */
+
+/**
+ * Creates an array from randomly picked values
+ * @param {number} length 
+ * @param {getValuesCallback} getValues 
+ * @returns {any[]}
+ */
 export function randArray (length, getValues) {
 	const arr = [];
-	for (let i = 0; i < length; ++i) arr.push(randItem(typeOf(getValues) === 'Function' ? getValues(i) : getValues));
+	for (let i = 0; i < length; ++i) arr.push(randItem(getValues(i)));
 	return arr;
 }
 
+/**
+ * Gets the chars for this index
+ * @callback getCharsCallback
+ * @param {number} index 
+ * @returns {string}
+ */
+
+/**
+ * Creates a string from randomly picked characters
+ * @param {number} length 
+ * @param {getCharsCallback} getChars 
+ * @returns {string}
+ */
 export function randString (length, getChars) {
 	return randArray(length, getChars).join('');
 }
 
+/**
+ * Generates a radom hex color, picks each character individually
+ * @param {string} max 
+ * @param {string} min 
+ * @returns {string}
+ */
 export function randHexColor (max = '#ffffff', min = '#000000') {
 	max = max.replace('#', '');
 	min = min.replace('#', '');
--- src/string.js
+++ src/string.js
@@ -1,48 +1,108 @@
 
+/**
+ * Capitalizes first letter
+ * @param {string} string 
+ * @returns {string}
+ */
 export function capitalize (string) {
 	return string[0].toUpperCase() + string.slice(1);
 }
 
+/**
+ * Decapitalizes first letter
+ * @param {string} string 
+ * @returns {string}
+ */
 export function decapitalize (string) {
 	return string[0].toLowerCase() + string.slice(1);
 }
 
+/**
+ * Formats string to PascalCase
+ * @param {string} string 
+ * @returns {string}
+ */
 export function toPascalCase (string) {
 	return string.split(' ').map(capitalize).join('');
 }
 
+/**
+ * Formats string to camelCase
+ * @param {string} string 
+ * @returns {string}
+ */
 export function toCamelCase (string) {
 	return decapitalize(toPascalCase(string));
 }
 
+/**
+ * Formats string to SNAKE_CASE
+ * @param {string} string 
+ * @returns {string}
+ */
 export function toSnakeCase (string) {
 	return string.split(' ').map((word) => word.toUpperCase()).join('_');
 }
 
+/**
+ * Formats string to kebab-case
+ * @param {string} string 
+ * @returns {string}
+ */
 export function toKebabCase (string) {
 	return string.split(' ').map((word) => word.toLowerCase()).join('-');
 }
 
+/**
+ * Array.prototype.splice but for strings
+ * @param {string} string 
+ * @param {number} index 
+ * @param {number?} remove 
+ * @param {string?} insert 
+ * @returns {string}
+ */
 export function spliceString (string, index, remove = 0, insert = '') {
 	return string.slice(0, index) + insert + string.slice(index + remove);
 }
 
+/**
+ * Truncates the end of a string
+ * @param {string} string 
+ * @param {number} length 
+ * @param {string?} replace 
+ * @returns {string}
+ */
 export function truncateString (string, length, replace = '...') {
 	return string.length > length
 		? string.slice(0, length - replace.length) + replace
 		: string;
 }
 
+/**
+ * Reverses a string efficiently
+ * @param {string} string 
+ * @returns {string}
+ */
 export function reverseString (string) {
 	let result = '';
 	for (const char of string) result = char + result;
 	return result;
 }
 
+/**
+ * Checks whether string is a basic email
+ * @param {string} string 
+ * @returns {boolean}
+ */
 export function isEmail (string) {
 	return /^(([a-z\d!#$%&'*+\/=?^_`{|}~-]+(\.[a-z\d!#$%&'*+\/=?^_`{|}~-]+)*)|(".+"))@([a-z\d-]+(\.[a-z\d]+)+|\[\d{1,3}(\.\d{1,3}){3}])$/i.test(string);
 }
 
+/**
+ * Finds a basic number in string
+ * @param {string} string 
+ * @returns {number}
+ */
 export function findNumber (string) {
 	return Number(string.match(/[-]?(\d+(\.\d+)?|\.\d+)(e[+-]?\d+)?/i)[0]);
 }
