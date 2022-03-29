e7ffe209348aeb0aee1511c373b500a887deefae
Eczbek <eczbek.void@gmail.com>
2022-03-29 08:05:29 -0400

    Add createWebSocketServer function

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 
 import { createReadStream } from 'fs';
-import { join } from 'path';
+import { join, parse } from 'path';
+import { EventEmitter } from 'events';
+import { randomUUID } from 'crypto';
+
+import { WebSocketServer } from 'ws';
 
 
 export function getURLPath (url) {
@@ -8,10 +12,41 @@ export function getURLPath (url) {
 }
 
 export function createRequestListener (servePath, echo = false) {
+	const { ext } = parse(servePath);
 	return (request, response) => {
 		if (echo && request.method === 'POST') return request.pipe(response);
-		const readStream = createReadStream(join(servePath, getURLPath(request.url)))
+		const readStream = createReadStream(ext ? servePath : join(servePath, getURLPath(request.url)))
 			.on('error', () => response.end('404'))
 			.on('open', () => readStream.pipe(response));
+	};
+}
+
+export function createWebSocketServer (server) {
+	const emitter = new EventEmitter();
+	const sockets = Object.create(null);
+	new WebSocketServer({ server })
+		.on('connection', (socket) => {
+			const id = randomUUID();
+			sockets[id] = socket;
+			emitter.emit('open', id);
+			socket
+				.on('close', () => {
+					delete sockets[id];
+					emitter.emit('close', id);
+				})
+				.on('message', (buffer) => {
+					emitter.emit('data', id, buffer.toString());
+					try {
+						emitter.emit('json', id, JSON.parse(buffer));
+					} catch {}
+				});
+		});
+	return {
+		events: emitter,
+		sendTo: (id, message) => sockets[id].send(JSON.stringify(message)),
+		sendToAll: (message) => {
+			const data = JSON.stringify(message);
+			Object.values(sockets).forEach((socket) => socket.send(data));
+		}
 	};
 }
