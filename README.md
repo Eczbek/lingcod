ac10eafb9d059551ca1f348697091ab43a2aa0f3
Eczbek <eczbek.void@gmail.com>
2022-04-02 17:34:01 -0400

    Fix imports

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# js-utils
+# lingcod
 ### JavaScript utility library
--- types/time.d.ts
+++ types/time.d.ts
@@ -1,5 +1,9 @@
 export declare const WEEK_DAYS: string[];
 export declare const MONTHS: string[];
 export declare const MILLIS_BEFORE_1970 = 62125920000000;
-export declare function destructDate(date?: Date, addMonth?: boolean): Map<string, number>;
+interface Dictionary {
+    [index: string]: any;
+}
+export declare function destructDate(date?: Date, addMonth?: boolean): Dictionary;
 export declare function createTimeFormat(format: string, prefix?: string, addMonth?: boolean): (date?: Date, utc?: boolean, names?: boolean) => string;
+export {};
