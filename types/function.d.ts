366216fbd3a94927c272e967d456d9b4240d5ea8
Eczbek <eczbek.void@gmail.com>
2022-04-02 20:02:31 -0400

    Remove compiled files

--- types/function.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function throttle(callback: Function, millis: number): (...args: any[]) => void;
-export declare function debounce(callback: Function, millis: number): {
-    (...args: any[]): void;
-    cancel(): void;
-};
-export declare function chain(...callbacks: Function[]): (...args: any[]) => Promise<any>;
