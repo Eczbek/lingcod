cc62fcb4ef61178de2a8cf0ee45d9a7e5b36d0a3
Eczbek <eczbek.void@gmail.com>
2022-04-02 21:04:37 -0400

    Make math return types default

--- src/math.ts
+++ src/math.ts
@@ -1,30 +1,30 @@
 
-export function norm (number: number, max: number, min = 0): number {
+export function norm (number: number, max: number, min = 0) {
 	return (number - min) / (max - min);
 }
 
-export function lerp (number: number, max: number, min = 0): number {
+export function lerp (number: number, max: number, min = 0) {
 	return number * (max - min) + min;
 }
 
-export function clamp (number: number, max: number, min = 0): number {
+export function clamp (number: number, max: number, min = 0) {
 	return Math.min(Math.max(number, min), max);
 }
 
-export function round (number: number, step = 1): number {
+export function round (number: number, step = 1) {
 	return Math.round(number / step) * step;
 }
 
-export function wrap (number: number, max: number, min = 0): number {
+export function wrap (number: number, max: number, min = 0) {
 	const diff = max - min;
 	return ((number - min) % diff + diff) % diff + min;
 }
 
-export function approxEqual (number1: number, number2: number, absEpsilon = 1e-12, relEpsilon = 1e-8): boolean {
+export function approxEqual (number1: number, number2: number, absEpsilon = 1e-12, relEpsilon = 1e-8) {
 	return Math.abs(number1 - number2) <= Math.min(absEpsilon, Math.max(Math.abs(number1), Math.abs(number2)) * relEpsilon);
 }
 
-export function* PI (digits = Infinity): Generator<number, void, unknown> {
+export function* PI (digits = Infinity) {
 	for (let a = 180n, b = 1n, c = 60n, d = 2n, i = 0; i <= digits; ++i, d += 1n) {
 		const x = ((d * 27n - 12n) * b + a * 5n) / (c * 5n);
 		const y = (d * 3n + 1n) * (d * 3n + 2n) * 3n;
@@ -35,7 +35,7 @@ export function* PI (digits = Infinity): Generator<number, void, unknown> {
 	}
 }
 
-export function getFactors (number: number): number[] {
+export function getFactors (number: number) {
 	const factors = [];
 	for (let i = 2; i <= Math.sqrt(number); ++i) {
 		const j = number / i;
